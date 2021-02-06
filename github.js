62ad183773b97e5a2925decd9d1352e9f1be6c78
Gladson Samuel S <74543240+Gladson-Samuel-S@users.noreply.github.com>
2021-02-06 08:57:06 +0530

    now fetches user's repos

--- github.js
+++ github.js
@@ -2,15 +2,20 @@ class Github{
     constructor() {
         this.client_id = '201bd676776d60733833';
         this.client_secret = '41ffe416ce9974c37b0eba6c75e14d3a70c9c711';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
     
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
     
         return {
-          profile
+          profile,repos
         }
     }
 
